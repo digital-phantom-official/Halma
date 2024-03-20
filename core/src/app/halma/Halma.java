d3ed6af37321b7e8ee8f4642cb67b6b4fffa8844
EHSAN LAPTOP <bssb.2015.17@gmail.com>
2024-03-20 12:35:33 +0500

    Added Sentry and user preferences

--- core/src/app/halma/Halma.java
+++ core/src/app/halma/Halma.java
@@ -1,5 +1,8 @@
 package app.halma;
 
+import app.halma.redesign.Settings;
+import app.halma.utils.ErrorMonitoring;
+import app.halma.utils.HalmaPreferences;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.I18NBundle;
@@ -10,24 +13,29 @@ import java.util.Locale;
 
 public class Halma extends Game {
 
-	private I18NBundle bundle;
-	
-	@Override
-	public void create () {
-		System.out.println("program start :)");
+    private I18NBundle bundle;
 
-		// Override to specific language with new Locale("de"), "en", etc.
-		Locale locale = Locale.getDefault();
-		System.out.println("your language is:");
-		System.out.println(locale);
-		I18NBundle.setSimpleFormatter(true); // Ensure same behaviour on GWT and other platforms
-		bundle = I18NBundle.createBundle(Gdx.files.internal("strings"), locale);
+    @Override
+    public void create() {
+        System.out.println("program start :)");
 
-		setScreen(new MainMenu(this));
-	}
+        if (HalmaPreferences.isMonitoring()) {
+            ErrorMonitoring.init();
+            ErrorMonitoring.startMonitoring();
+        }
 
-	I18NBundle getBundle() {
-		return bundle;
-	}
+        // Override to specific language with new Locale("de"), "en", etc.
+        Locale locale = Locale.getDefault();
+        System.out.println("your language is:");
+        System.out.println(locale);
+        I18NBundle.setSimpleFormatter(true); // Ensure same behaviour on GWT and other platforms
+        bundle = I18NBundle.createBundle(Gdx.files.internal("strings"), locale);
+
+        setScreen(HalmaPreferences.isFirstRun() ? new Settings(this) : new MainMenu(this));
+    }
+
+    I18NBundle getBundle() {
+        return bundle;
+    }
 
 }
